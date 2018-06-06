8a920b6137fd0090140955f6be32dd98d93d2d0e
Wesley Vicente <wesley1535@hotmail.com>
2018-06-06 02:30:58 -0300

    Update principal.java

--- principal.java
+++ principal.java
@@ -1,25 +1,24 @@
 package Ordenacao;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
+
 
 public class principal extends algoritmosOrdenacao {
-
+static Scanner scan = new Scanner(System.in);
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		int ch;
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		do {
-			System.out.println("\n\n SELECIONE UM ALGORITMO DE ORDENAÇÃO!\n\n1.Bubble Sort\n2.Insertion Sort."
+			System.out.println("\n\n SELECIONE UM ALGORITMO DE ORDENAÃ‡ÃƒO!\n\n1.Bubble Sort\n2.Insertion Sort."
 					+ "\n3.Selection Sort.\n4.Merge Sort.\n5.Quick Sort.\n6.Exit.");
-			ch = Integer.parseInt(br.readLine());
+			ch = scan.nextInt();
 			if (ch == 6)
 				return;
-			System.out.print("\n Entre com a quantidade de Números Inteiros:");
-			int n = Integer.parseInt(br.readLine());
+			System.out.print("\n Entre com a quantidade de NÃºmeros Inteiros:");
+			int n = scan.nextInt();
 			int a[] = new int[n];
 			for (int i = 0; i < n; i++) {
-				a[i] = Integer.parseInt(br.readLine());
+				a[i] = scan.nextInt();
 			}
 			switch (ch) {
 			case 1:
